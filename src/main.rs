1db674ed03d1c3342d955ff8bd1f997bf7525421
Davy Tran <davytran1993@gmail.com>
2024-08-08 16:48:47 +0200

    Add connection to database

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
+use diesel::{r2d2, PgConnection};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv::dotenv().ok();
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let manager = r2d2::ConnectionManager::<PgConnection>::new(database_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+    let domain = std::env::var("DOMAIN").unwrap_or_else(|_| "localhost".to_owned());
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(pool.clone()))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
