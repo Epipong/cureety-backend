a9bf4633167eb9a8e985749c658104831c8672ed
Davy Tran <davytran1993@gmail.com>
2024-08-11 21:42:14 +0200

    Enum to fix

--- src/users/handler.rs
+++ src/users/handler.rs
@@ -1,5 +1,5 @@
 use actix_web::{get, web, HttpResponse, Responder};
-use diesel::{query_dsl::methods::SelectDsl, RunQueryDsl, SelectableHelper};
+use diesel::{QueryDsl, RunQueryDsl, SelectableHelper};
 
 use crate::{
     schema::users,
@@ -9,10 +9,13 @@ use crate::{
 #[get("/users")]
 pub async fn users_list(pool: web::Data<Pool>) -> impl Responder {
     let mut conn = pool.get().unwrap();
-    let items = users::table.select(User::as_select()).load::<User>(&mut conn).unwrap();
+    let items = users::table
+        .select(User::as_select())
+        .load::<User>(&mut conn)
+        .unwrap();
 
     HttpResponse::Ok().json(items)
-}
+}   
 
 pub fn config(conf: &mut web::ServiceConfig) {
     let scope = web::scope("/api").service(users_list);
