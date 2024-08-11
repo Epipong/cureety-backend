938757bba978394511a305a5d56e44e1ba2bd8fc
Davy Tran <davytran1993@gmail.com>
2024-08-12 00:25:36 +0200

    Add AppState and init create users post

--- src/users/handler.rs
+++ src/users/handler.rs
@@ -1,10 +1,12 @@
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{body, get, post, web, HttpResponse, Responder};
 use diesel::{QueryDsl, RunQueryDsl, SelectableHelper};
 
 use crate::{
     schema::users,
     users::model::{Pool, User},
 };
+
+use super::model::AppState;
 
 #[get("/users")]
 pub async fn users_list(pool: web::Data<Pool>) -> impl Responder {
@@ -15,7 +17,17 @@ pub async fn users_list(pool: web::Data<Pool>) -> impl Responder {
         .expect("select");
 
     HttpResponse::Ok().json(items)
-}   
+}
+
+#[post("/users")]
+pub async fn create_user(
+    mut body: web::Json<User>,
+    data: web::Data<AppState>
+) -> impl Responder {
+    let mut vec = data.user_db.lock().unwrap();
+
+    HttpResponse::Ok().json(0)
+}
 
 pub fn config(conf: &mut web::ServiceConfig) {
     let scope = web::scope("/api").service(users_list);
